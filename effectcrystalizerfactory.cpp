291b7743270aa42506b5dae0e10e6f8e9f2cd96b
Greedysky <greedysky@163.com>
2025-11-07 15:14:14 +0800

    feat: Support for qmmp 0.x Qt4

--- effectcrystalizerfactory.cpp
+++ effectcrystalizerfactory.cpp
@@ -46,3 +46,8 @@ QString EffectCrystalizerFactory::translation() const
 {
     return QString();
 }
+
+#if QT_VERSION < QT_VERSION_CHECK(5,0,0)
+#include <QtPlugin>
+Q_EXPORT_PLUGIN2(crystalizer, EffectCrystalizerFactory)
+#endif
